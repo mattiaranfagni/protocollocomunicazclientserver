3bbe523bf5d097e72ae2348ae07aa6d490384001
Ranfagni Mattia <ranfagni.mattia@itismeucci.com>
2017-02-13 09:21:48 +0100

    Primo commit, vuoto 13/02/17

---
+++ TCPServer/src/tcpserver/TCPServer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tcpserver;
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author Margherita Mugnai
+ */
+public class TCPServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        // TODO code application logic here
+        String clientSentence;
+        String capitalizedSentence;
+        ServerSocket welcomeSocket=new ServerSocket(6789);
+        while(true){
+            Socket connectionSocket=welcomeSocket.accept();
+            BufferedReader inFromClient=new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+            DataOutputStream outToClient=new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence=inFromClient.readLine();
+            capitalizedSentence=clientSentence.toUpperCase()+'\n';
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
+    
+}
